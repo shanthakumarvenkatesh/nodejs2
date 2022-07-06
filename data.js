def665d6bb9cd1bc97a89f00f3a9233fce43c531
Shanthakumar <93003141+shanthakumarvenkatesh@users.noreply.github.com>
2022-07-06 18:16:31 +0530

    Update data.js

--- data.js
+++ data.js
@@ -10,27 +10,58 @@
 // }
 
 
-const json1=[
-    {
-        name:"shanth",
-        email:"shanth@gmail.com"
-    }
-]
+// const json1=[
+//     {
+//         name:"shanth",
+//         email:"shanth@gmail.com"
+//     }
+// ]
 
 
-const json2=[
-    {
-        name:"vk",
-        email:"sth@gmail.com"
-    }
-]
-const add =function(a,b){
-    return a+b;
+// const json2=[
+//     {
+//         name:"vk",
+//         email:"sth@gmail.com"
+//     }
+// ]
+// const add =function(a,b){
+//     return a+b;
+// }
+// // exports.json1 =json1;
+// // exports.json2 =json2;
+// module.exports={
+// json1:json1,
+// json2:json2,
+// add:add
+// }
+
+const express=require('express')
+const app=express();
+const middleware1 = function(req,res,next){ 
+    console.log('middleWare 1 ');
+    next();
 }
-// exports.json1 =json1;
-// exports.json2 =json2;
-module.exports={
-json1:json1,
-json2:json2,
-add:add
+
+app.use(middleware1); 
+
+const middleware2 = function(req,res,next){
+    console.log('middleware 2');
 }
+
+app.get('/',middleware2,function(req,res){
+    res.send("Home page")
+})
+
+app.get('/1',function(req,res){
+    res.send("One")
+})
+
+app.get('/2',middleware2,function(req,res){
+    res.send("Two")
+})
+
+app.get('/3',function(req,res){
+    res.send("Three")
+})
+
+app.listen(6666);
